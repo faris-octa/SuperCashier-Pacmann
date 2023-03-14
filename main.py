e42d22df04caf18271eb1b71dd07beab66ea3bb8
faris-octa <m.faris.octa@gmail.com>
2023-03-14 22:10:25 +0700

    menambahkan fungsi update_item_name

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import pandas as pd
 #### dummy databases ####
 df = pd.DataFrame({'trnsct_id': [1, 2]})
 
+
+
+
+
 class Transaction:
     # constructor buat dataframe kosong sebagai keranjang belanja
     def __init__(self):
         self.cart = pd.DataFrame(columns=['Nama Item', 'Jumlah Item', 'Harga/Item'])
-
-    def perkenalan(self):
-        print(f'Nomor transaksi sekarang: {self.trnsct_id}')
 
     # add item
     def add_item(self):
@@ -24,7 +25,15 @@ class Transaction:
         print(f"Berhasil memasukkan {nama_item} seharga Rp. {harga_per_item} sebanyak {jumlah_item} buah ke keranjang")
         print(self.cart)
 
-        
+    # update item
+    def update_item_name(self, nama_item, nama_item_updated):
+        ### cari nama item di self.cart['Nama Item'] pake for trus ganti jadi nama_item_updated
+        for item in self.cart['Nama Item']:
+            if item == nama_item:
+                self.cart.loc[self.cart['Nama Item'] == nama_item, 'Nama Item'] = nama_item_updated
+        print(self.cart)
+
+
 
     
 
@@ -32,3 +41,5 @@ class Transaction:
 
 test1 = Transaction()
 test1.add_item()
+test1.add_item()
+test1.update_item_name('tepung', 'tepung terigu')
