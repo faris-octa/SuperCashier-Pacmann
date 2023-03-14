a138467cee9120785a72d95d106c1d332caa441a
faris-octa <m.faris.octa@gmail.com>
2023-03-14 21:30:30 +0700

    menambahkan konstruktor berupa df kosong dan method add_item

--- main.py
+++ main.py
@@ -5,22 +5,30 @@ import pandas as pd
 df = pd.DataFrame({'trnsct_id': [1, 2]})
 
 class Transaction:
-    # constructor buat id transaksi = +1 dari last id
+    # constructor buat dataframe kosong sebagai keranjang belanja
     def __init__(self):
-        self.trnsct_id = df['trnsct_id'].iloc[-1] + 1
+        self.cart = pd.DataFrame(columns=['Nama Item', 'Jumlah Item', 'Harga/Item'])
 
     def perkenalan(self):
         print(f'Nomor transaksi sekarang: {self.trnsct_id}')
 
     # add item
-    def add_item(nama_item, jumlah_item, harga_per_item):
+    def add_item(self):
         nama_item = input('Nama Item: ')
         jumlah_item = int(input('Jumlah: '))
         harga_per_item = int(input('Harga: '))
+        new_item = {'Nama Item': nama_item,
+                    'Jumlah Item': jumlah_item,
+                    'Harga/Item': harga_per_item}
+        self.cart = self.cart.append(new_item, ignore_index=True)
+        print(f"Berhasil memasukkan {nama_item} seharga Rp. {harga_per_item} sebanyak {jumlah_item} buah ke keranjang")
+        print(self.cart)
+
+        
 
     
 
     
 
 test1 = Transaction()
-test1.perkenalan()
+test1.add_item()
