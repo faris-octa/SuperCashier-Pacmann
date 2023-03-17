a3fe7c320ef21bd8175913085f66d952db7a1dbd
Faris Octa <m.faris.octa@gmail.com>
2023-03-17 13:58:12 +0700

    completing update item feature

--- main.py
+++ main.py
@@ -12,27 +12,52 @@ class Transaction:
     # constructor buat dataframe kosong sebagai keranjang belanja
     def __init__(self):
         self.cart = pd.DataFrame(columns=['Nama Item', 'Jumlah Item', 'Harga/Item'])
+        print("")
+        print("Selamat datang di e-Mart")
+        print("Silakan masukkan barang belanja anda ke cart...")
 
-    # add item
+    #### add item feature ####
     def add_item(self):
         nama_item = input('Nama Item: ')
         jumlah_item = int(input('Jumlah: '))
         harga_per_item = int(input('Harga: '))
-        new_item = {'Nama Item': nama_item,
-                    'Jumlah Item': jumlah_item,
-                    'Harga/Item': harga_per_item}
-        self.cart = self.cart.append(new_item, ignore_index=True)
+        new_item = pd.DataFrame({'Nama Item': [nama_item],
+                                 'Jumlah Item': [jumlah_item],
+                                 'Harga/Item': [harga_per_item]})
+        self.cart = pd.concat([self.cart, new_item], ignore_index=True)
         print(f"Berhasil memasukkan {nama_item} seharga Rp. {harga_per_item} sebanyak {jumlah_item} buah ke keranjang")
         print(self.cart)
+        print("")
+    ##########################
 
-    # update item
+    #### update item features ####
     def update_item_name(self, nama_item, nama_item_updated):
         ### cari nama item di self.cart['Nama Item'] pake for trus ganti jadi nama_item_updated
         for item in self.cart['Nama Item']:
             if item == nama_item:
                 self.cart.loc[self.cart['Nama Item'] == nama_item, 'Nama Item'] = nama_item_updated
+        print(f"Item {nama_item} telah diupdate menjadi {nama_item_updated}")
         print(self.cart)
+        print("")
 
+    def update_item_qty(self, nama_item, qty_item_updated):
+        for item in self.cart['Nama Item']:
+            if item == nama_item:
+                self.cart.loc[self.cart['Nama Item'] == nama_item, 'Jumlah Item'] = qty_item_updated
+        print(f"Jumlah Item {nama_item} telah diupdate menjadi {qty_item_updated} buah")
+        print(self.cart)
+        print("")
+
+    def update_item_price(self, nama_item, price_item_updated):
+        for item in self.cart['Nama Item']:
+            if item == nama_item:
+                self.cart.loc[self.cart['Nama Item'] == nama_item, 'Harga/Item'] = price_item_updated
+        print(f"Harga Item {nama_item} telah diupdate menjadi Rp. {price_item_updated}")
+        print(self.cart)
+        print("")
+    ##############################
+
+    
 
 
     
@@ -41,5 +66,6 @@ class Transaction:
 
 test1 = Transaction()
 test1.add_item()
-test1.add_item()
 test1.update_item_name('tepung', 'tepung terigu')
+test1.update_item_qty('tepung terigu', 200)
+test1.update_item_price('tepung terigu', 10000000000)
