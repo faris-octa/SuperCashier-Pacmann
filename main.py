d1e564a35bff4293e9e2b072ee74d74118f712d7
faris-octa <m.faris.octa@gmail.com>
2023-03-26 17:20:28 +0700

    add delete and check feature

--- main.py
+++ main.py
@@ -81,9 +81,8 @@ class Transaction:
     #### empty cart feature ####
     def reset_transaction(self):
         self.cart = self.cart.iloc[0:0]
-        if self.cart.empty:
-            print("\nBerhasil mengosongkan keranjang belanja, mari berbelanja!")
-    ############################
+        return self.cart
+    #############################
 
     #### check order feature ####
     def check_order(self):
@@ -123,7 +122,7 @@ if __name__ == "__main__":
         print("\nPilih fungsi yang ingin dieksekusi:")
         print("1. Masukkan barang ke keranjang belanja")
         print("2. Update barang di keranjang belanja")
-        print("3. Hapus barang dari keranjang belanja")
+        print("3. Keluarkan barang dari keranjang belanja")
         print("4. Check keranjang belanja")
         print("5. Checkout / Keluar")
 
@@ -135,9 +134,9 @@ if __name__ == "__main__":
             print(trnsct_123.add_item())
 
         elif choice == '2':
-            nama_item = input('Nama Item yang ingin diubah: ')
+            nama_item = input('Nama barang yang ingin diubah: ')
             if nama_item not in trnsct_123.cart['Nama Item'].to_list():
-                print(f"\nTidak ada item {nama_item} di keranjang")
+                print(f"\nTidak ada {nama_item} di keranjang")
             else:
                 print("\nApa yang ingin diupdate:")
                 print("1. Nama Barang")
@@ -148,13 +147,37 @@ if __name__ == "__main__":
                     updated_item = input(f'{nama_item} diubah menjadi: ')
                     trnsct_123.update_item_name(nama_item, updated_item)
                 elif choice_update == "2":
-                    updated_item_qty = int(input(f"jumlah item {nama_item} diubah menjadi: "))
+                    updated_item_qty = int(input(f"jumlah barang {nama_item} diubah menjadi: "))
                     trnsct_123.update_item_qty(nama_item, updated_item_qty)
                 elif choice_update == '3':
-                    updated_item_price = int(input(f"harga item {nama_item} diubah menjadi: "))
+                    updated_item_price = int(input(f"harga barang {nama_item} diubah menjadi: "))
                     trnsct_123.update_item_price(nama_item, updated_item_price)
-        elif choice == 'x':
-            trnsct_123.check_order()
+                else:
+                    print("Pilihan tidak valid")
+
+        elif choice == '3':
+            pass
+            print("\nPilih opsi:")
+            print("1. Keluarkan satu barang")
+            print("2. Kosongkan keranjang belanja")
+            choice_remove = input("Masukkan pilihan anda (1 or 2): ")
+            if choice_remove == "1":
+                nama_item = input('Nama barang yang ingin dikeluarkan: ')
+                if nama_item in trnsct_123.cart['Nama Item'].to_list():
+                    trnsct_123.delete_item(nama_item)
+                else:
+                    print(f"\nTidak ada item {nama_item} di keranjang")
+            elif choice_remove == '2':
+                trnsct_123.reset_transaction()
+            else:
+                print("Pilihan tidak valid")
+
+
+        elif choice == '4':
+            if trnsct_123.cart.empty:
+                continue
+            else:
+                trnsct_123.check_order()
         print(trnsct_123.cart['Nama Item'].to_list())
 
         # if choice == '1':
